c6337bb066795fe2646a6a253296f316f7695610
Mert-55 <mcagiran.arden@gmail.com>
2026-01-03 13:06:54 +0300

    X-20 Implement Customers Service Endpoints (Tree Map & Area Chart)

--- src/services/types/dto/index.ts
+++ src/services/types/dto/index.ts
@@ -1,7 +1,7 @@
-export { type SegmentMetricsDTO } from './SegmentMetricsDTO';
-export { type SegmentTreeItemDTO } from './SegmentTreeItemDTO';
-export { type StockItemDTO } from './StockItemDTO';
 export {
   type RevenueBySegmentDTO,
   type RevenueTimeSeriesItemDTO,
 } from './RevenueTimeSeriesItemDTO';
+export { type SegmentMetricsDTO } from './SegmentMetricsDTO';
+export { type SegmentTreeItemDTO } from './SegmentTreeItemDTO';
+export { type StockItemDTO } from './StockItemDTO';
